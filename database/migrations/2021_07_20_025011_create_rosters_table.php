5ce23a28b20800ccd9241b3963f57968b18d475d
mk.farhat@hotmail.com <mk.farhat@hotmail.com>
2021-07-28 09:53:06 +1000

    main

---
+++ database/migrations/2021_07_20_025011_create_rosters_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRostersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rosters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('userId');
+            $table->date('startDate');
+            $table->date('endDate');
+            $table->time('startTime');
+            $table->time('endTime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rosters');
+    }
+}
